06daee0d9df8dc746fe0029ec3c951dc8d28a8ea
shin <lijiawei_88@qq.com>
2017-08-31 09:33:01 +0800

    首次提交测试

---
+++ src/main/java/org/zapx/lab/ZapxLabApplication.java
@@ -0,0 +1,15 @@
+package org.zapx.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Shin on 2017/8/31.
+ */
+@SpringBootApplication
+public class ZapxLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZapxLabApplication.class);
+    }
+}
