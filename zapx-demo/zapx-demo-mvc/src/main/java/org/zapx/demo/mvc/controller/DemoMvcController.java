3d6203735586b494adc0945eddacc454a230d2cd
lijiawe1 <lijiawe_88@qq.com>
2017-11-06 07:21:48 +0800

    部署nexus

--- zapx-demo/zapx-demo-mvc/src/main/java/org/zapx/demo/mvc/controller/DemoMvcController.java
+++ zapx-demo/zapx-demo-mvc/src/main/java/org/zapx/demo/mvc/controller/DemoMvcController.java
@@ -2,6 +2,9 @@ package org.zapx.demo.mvc.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.h2.H2ConsoleAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -10,7 +13,7 @@ import java.util.Map;
 /**
  * Created by Administrator on 2017/9/9 0009.
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class, H2ConsoleAutoConfiguration.class})
 @RestController
 @RequestMapping("demo/mvc")
 public class DemoMvcController {
